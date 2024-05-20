76aef01022b710f567046e2fbb81d0bae399d302
Prajwal <imprjde7@gmail.com>
2024-05-20 23:12:20 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "cdn.iconscout.com",
+      "www.chitkara.edu.in",
+      "source.unsplash.com",
+      "images.unsplash.com",
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com",
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/audio",
+          outputPath: `${isServer ? "../" : ""}static/audio/`,
+          name: "[name].[ext]",
+          esModule: false,
+        },
+      },
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
