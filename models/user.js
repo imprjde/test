af0b182e5abb091a4a3fad8390c7f7727621309d
Prajwal <imprjde7@gmail.com>
2024-05-21 09:59:43 +0530

    user schema Bug fix

--- models/user.js
+++ models/user.js
@@ -1,49 +1,18 @@
-import { connectMongoDB } from "@/utils/mongodbConnect";
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import User from "@/models/user";
+import mongoose, { Schema, models } from "mongoose";
 
-const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn({ user, account }) {
-      if (account.provider === "google") {
-        const { name, email } = user;
-        try {
-          await connectMongoDB();
-          const userExists = await User.findOne({ email });
-
-          if (!userExists) {
-            const res = await fetch("http://localhost:3000/api/user", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                name,
-                email,
-              }),
-            });
-
-            if (res.ok) {
-              return user;
-            }
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
-      return user;
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
     },
   },
-};
+  { timestamps: true }
+);
 
-const User = NextAuth(authOptions);
-
-export { User as GET, User as POST };
+const User = models.User || mongoose.model("User", userSchema);
+export default User;
